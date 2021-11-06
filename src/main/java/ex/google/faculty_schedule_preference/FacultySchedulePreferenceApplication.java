459e235768b822d7d34c0cb75e68a9d5758e5390
RYAN ABUKARN <61808006+RyanAbukarn@users.noreply.github.com>
2021-11-06 12:12:25 -0700

    first commit

---
+++ src/main/java/ex/google/faculty_schedule_preference/FacultySchedulePreferenceApplication.java
@@ -0,0 +1,13 @@
+package ex.google.faculty_schedule_preference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultySchedulePreferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacultySchedulePreferenceApplication.class, args);
+	}
+
+}
