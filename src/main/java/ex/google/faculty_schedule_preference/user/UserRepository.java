0c67496a911e60b86c2e1a1aca79f80104e46f74
Jozsef123 <jfeher02@gmail.com>
2021-11-30 14:02:27 -0800

    added update roles page and security files

--- src/main/java/ex/google/faculty_schedule_preference/user/UserRepository.java
+++ src/main/java/ex/google/faculty_schedule_preference/user/UserRepository.java
@@ -2,6 +2,7 @@ package ex.google.faculty_schedule_preference.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByUsername(String username);
 }
