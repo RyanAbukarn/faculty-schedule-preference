e64746b33a540b7a3db68721d0ef73237c58ef05
RYAN ABUKARN <61808006+RyanAbukarn@users.noreply.github.com>
2022-02-24 18:30:08 -0800

    route issue

--- src/main/java/ex/google/faculty_schedule_preference/registration/RegistrationController.java
+++ src/main/java/ex/google/faculty_schedule_preference/registration/RegistrationController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "user/registration")
+@RequestMapping(path = "users/registration")
 @AllArgsConstructor
 @EqualsAndHashCode
 @ToString
