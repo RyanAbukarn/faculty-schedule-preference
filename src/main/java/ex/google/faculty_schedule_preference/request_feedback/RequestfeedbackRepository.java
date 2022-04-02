547940ca1284f9e3bfecdef73beecbfa6ceb8576
RYAN ABUKARN <61808006+RyanAbukarn@users.noreply.github.com>
2022-04-02 11:47:13 -0700

    file name change not updated.

--- src/main/java/ex/google/faculty_schedule_preference/request_feedback/RequestfeedbackRepository.java
+++
@@ -1,7 +0,0 @@
-package ex.google.faculty_schedule_preference.request_feedback;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RequestFeedbackRepository extends JpaRepository<RequestFeedback, Long> {
-
-}
